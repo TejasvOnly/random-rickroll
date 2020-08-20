64cc0d8f5e9462cb3d892bb1cc6763e8c437f957
Tejasv Sharma <9414297797.ts@gmail.com>
2020-08-20 06:08:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const percentage = core.getInput('percentage');
+    const message;
+    if(percentage > Math.floor(Math.random() * 100)){
+        message = '![rickroll](https://user-images.githubusercontent.com/37572049/90699500-0cc3ec00-e2a1-11ea-8d13-989526e86b0e.gif)';
+        console.log('Gottem!!');
+        
+    }else{
+        console.log('Fine you win this time.');
+        message = core.getInput('message');
+        if(message == ''){
+            return;
+        }
+    }
+
+    const github_token = core.getInput('GITHUB_TOKEN');
+
+    const context = github.context;
+    if (context.payload.issue == null) {
+        core.setFailed('No issue found.');
+        return;
+    }
+    const issue_number = context.payload.issue.number;
+
+    const octokit = new github.GitHub(github_token);
+    const new_comment = octokit.issues.createComment({
+        ...context.repo,
+        issue_number: issue_number,
+        body: message
+      });
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
